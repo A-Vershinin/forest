ec1e963d762ace7711bd5fcabb20b419c279a8ec
Anatoliy Vershinin <cata07@yandex.ru>
2018-02-10 22:30:57 +0200

    change menu

--- app/js/modules/menu.js
+++ app/js/modules/menu.js
@@ -1,8 +1,10 @@
 function menu () {
   const hamburger = document.querySelector(".header__toggle");
   const hamburgerActive = "is-active";
-  const wrapperMenu = document.querySelector(".wrapper__menu");
-  const wrapperMenuActive = "wrapper__menu--show";
+  // const wrapperMenu = document.querySelector(".wrapper__menu");
+  const wrapperMenu = document.querySelector(".header__menu");
+  // const wrapperMenuActive = "wrapper__menu--show";
+  const wrapperMenuActive = "header__menu--show";
 
   if (hamburger) {
     hamburger.addEventListener("click", function () {
